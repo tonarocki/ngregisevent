ad464bac416f942caedbf6eceb74884caadfc27c
kittipoom <tonarocki@hotmail.com>
2018-01-19 17:22:37 +0700

    form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,21 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 
 import { ParamMap, Router, ActivatedRoute, RoutesRecognized } from '@angular/router';
-import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import { OnDestroy, AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
 import { RegisService } from '../share/regis.service';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit, OnDestroy {
 
+
+
+export class RegisterComponent implements OnInit, OnDestroy, AfterViewInit {
   private sub: any = null;
   param: any;
   regislistid: any;
   regisTitle: String;
   regisDetail: String;
+  public registeruser: any;
+
+
+
+  displayedColumns = ['stdid', 'prefix', 'fname', 'lname'];
+  dataSource = new MatTableDataSource<any>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+
+
+
   constructor(private route: ActivatedRoute, private router: Router, private registerservice: RegisService) {
     this.param = this.route.snapshot.params.id;
   }
@@ -26,6 +42,11 @@ export class RegisterComponent implements OnInit, OnDestroy {
         console.log(regislistid);
         this.regisTitle = regislistid.title;
         this.regisDetail = regislistid.detail;
+
+        this.registeruser = regislistid.RegisDetails;
+        console.log(this.registeruser);
+
+
       }, (err) => {
         console.log(err);
         this.router.navigate(['/home']);
@@ -33,10 +54,18 @@ export class RegisterComponent implements OnInit, OnDestroy {
     );
   }
 
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
   ngOnDestroy() {
     if (this.sub != null) {
         this.sub.unsubscribe();
     }
   }
-
 }
+
+
+
+const ELEMENT_DATA: any = [this.registeruser]  ;
+console.log(this.registeruser);
